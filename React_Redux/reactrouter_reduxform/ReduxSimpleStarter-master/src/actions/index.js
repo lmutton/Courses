5dee3b87f080f398477418751a61d5b933f0495b
lmutton <lornamutton@gmail.com>
2017-08-10 15:03:03 +0200

    able to add new post and redirect to post list

--- React_Redux/reactrouter_reduxform/ReduxSimpleStarter-master/src/actions/index.js
+++ React_Redux/reactrouter_reduxform/ReduxSimpleStarter-master/src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POSTS = 'create_post';
+
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY ='?key=lmutt010203';
 
@@ -11,4 +13,15 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+  .then(() => callback());
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
--- React_Redux/reactrouter_reduxform/ReduxSimpleStarter-master/src/components/posts_new.js
+++ React_Redux/reactrouter_reduxform/ReduxSimpleStarter-master/src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm} from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 //const { meta: { touched, error } } = field; condenses
 class PostsNew extends Component {
@@ -24,7 +26,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values){
-    console.log(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render () {
@@ -78,7 +82,9 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null,{ createPost})(PostsNew)
+);
 
 
 // 'PostsNewForm' need to be unique
